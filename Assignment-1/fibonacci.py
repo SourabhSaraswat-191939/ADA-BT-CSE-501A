d7d251a1a5d57fba1817dcaf6fdba82e22c6d517
Sourabh Saraswat <saraswatsourabh5@gmail.com>
2021-09-13 23:15:44 +0530

    Fibonacci Series Assignment-1
    Signed-off-by: Sourabh Saraswat <saraswatsourabh5@gmail.com>

--- Assignment-1/fibonacci.py
+++ Assignment-1/fibonacci.py
@@ -3,15 +3,17 @@ import time
 def fib(n):
     if n==0 or n==1:
         return n
-    ans = fib(n-1) + fib(n-2)
+    ans = fib(n-1) + fib(n-2)   # using recursion here
     return ans
 
 n = int(input("Enter number to find Fibonacci Series -> "))
 
+# using time library to record the start time.
 start = time.time()
 for i in range(n):
-    print(fib(i))
+    print(fib(i))   # printing n terms of fabonacci series one by one using for loop.
 
+# using time library to record the end time.
 end = time.time()
 
 print(f"Runtime of the program is {end - start} seconds")
--- Assignment-1/fibonacci_using_DP.py
+++ Assignment-1/fibonacci_using_DP.py
@@ -2,15 +2,19 @@ import time
 
 n = int(input("Enter number to find Fibonacci Series -> "))
 
+# creating a variable to store data of calculated terms, so that we can use dynamic programming approach.
 Dp_arr = [-1]*(n+1)
-Dp_arr[0] = 0
-Dp_arr[1] = 1
+Dp_arr[0] = 0   # as 1st term of fibonacci is 0.
+Dp_arr[1] = 1   # as 2nd term of fibonacci is 1.
 
-start = time.time()
+# using time library to record the start time.
+start = time.time()   
 for i in range(2,n+1):
     Dp_arr[i] = Dp_arr[i-1] + Dp_arr[i-2]
 
+# using time library to record the end time.
 end = time.time()
 
+# printing n terms of fabonacci series.
 print(Dp_arr)
 print(f"Runtime of the program is {end - start} seconds")
